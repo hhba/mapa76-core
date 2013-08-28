bd22877a2fc35dc77cccae0c739b647d390e9d4f
Damián Silvani <munshkr@gmail.com>
2013-08-28 01:11:00 -0300

    Remove context and href again, they are fucking slow

--- lib/mapa76/core/lib/document_exporter.rb
+++ lib/mapa76/core/lib/document_exporter.rb
@@ -3,22 +3,22 @@ require 'csv'
 module DocumentExporter
   def export_people
     export(:people_found,
-           %w{ _id document_id text context href lemma tag prob pos sentence_pos })
+           %w{ _id document_id text lemma tag prob pos sentence_pos })
   end
 
   def export_dates
     export(:dates_found,
-           %w{ _id document_id text context href lemma tag prob pos sentence_pos time })
+           %w{ _id document_id text lemma tag prob pos sentence_pos time })
   end
 
   def export_places
     export([:places_found, :addresses_found],
-           %w{ _id document_id text context href lemma tag prob pos sentence_pos lat lng })
+           %w{ _id document_id text lemma tag prob pos sentence_pos lat lng })
   end
 
   def export_organizations
     export(:organizations_found,
-           %w{ _id document_id text context href lemma tag prob pos sentence_pos })
+           %w{ _id document_id text lemma tag prob pos sentence_pos })
   end
 
 private
